c7034f831dbdb9d969db572c985f06e187339023
guru-bandike <guru.bandike@gmail.com>
2024-04-06 09:30:34 +0530

    Combine start and stop buttons into a single button for dual functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // Selecting DOM elements for the stopwatch
-const body = document.querySelector("body");
-const container = document.querySelector("#container");
-const barElementsContainer = document.querySelector("#bar-elements");
-const startButton = document.querySelector("#start-button");
-const stopButton = document.querySelector("#stop-button");
-const resetButton = document.querySelector("#reset-button");
-const minutesAndSecondsElement = document.querySelector("#minutesAndSeconds");
-const millisecondsElement = document.querySelector("#milliseconds");
-const themeToggleButton = document.querySelector("#theme-toggle");
-const tickingSound = new Audio("src/audios/ticking-sound.mp3");
+const body = document.querySelector('body');
+const mainContainer = document.querySelector('main');
+const barElementsContainer = document.querySelector('#bar-elements');
+const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
+const resetButton = document.querySelector('.reset-button');
+const minutesAndSecondsElement = document.querySelector('#minutesAndSeconds');
+const millisecondsElement = document.querySelector('#milliseconds');
+const tickingSound = new Audio('src/audios/ticking-sound.mp3');
 
 // Initializing stopwatch time variables
 let minutes = 0;
@@ -19,19 +18,43 @@ let timeIntervalId;
 // Creating visual bars for each second
 const barElements = [];
 for (let currbarNum = 1; currbarNum <= 60; currbarNum++) {
-  barElements.push(
-    `<span id="barId${currbarNum}" style="--index:${currbarNum};"><p></p></span>`
-  );
+  barElements.push(`<span id="barId${currbarNum}" style="--index:${currbarNum};"><p></p></span>`);
 }
 
 // Inserting bars into the DOM
-barElementsContainer.insertAdjacentHTML("afterbegin", barElements.join(""));
+barElementsContainer.insertAdjacentHTML('afterbegin', barElements.join(''));
 
-// Event listeners for the stopwatch buttons
-startButton.addEventListener("click", startInterval);
-stopButton.addEventListener("click", stopInterval);
-resetButton.addEventListener("click", resetInterval);
-themeToggleButton.addEventListener("click", toggleTheme);
+// Event listener for handling click events on the document
+document.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target === startButton) {
+    startInterval();
+    target.textContent = 'Stop';
+    target.classList.toggle('start-button');
+    target.classList.toggle('stop-button');
+    return;
+  }
+
+  if (target === stopButton) {
+    stopInterval();
+    target.textContent = 'Start';
+    target.classList.toggle('start-button');
+    target.classList.toggle('stop-button');
+    return;
+  }
+
+  if (target === resetButton) {
+    resetInterval();
+    return;
+  }
+
+  const toggleThemeButton = target.closest('.toggle-theme');
+  if (toggleThemeButton) {
+    toggleTheme();
+    return;
+  }
+});
 
 // Function to start the stopwatch
 function startInterval() {
@@ -51,9 +74,8 @@ function startInterval() {
     }
 
     // Updating the DOM with the new time
-    minutesAndSecondsElement.textContent =
-      formatTime(minutes) + ":" + formatTime(seconds);
-    millisecondsElement.textContent = "." + formatTime(milliseconds);
+    minutesAndSecondsElement.textContent = formatTime(minutes) + ':' + formatTime(seconds);
+    millisecondsElement.textContent = '.' + formatTime(milliseconds);
   }, 10);
 }
 
@@ -69,13 +91,13 @@ function resetInterval() {
   minutes = 0;
   seconds = 0;
   milliseconds = 0;
-  minutesAndSecondsElement.textContent = "00:00";
-  millisecondsElement.textContent = ".00";
+  minutesAndSecondsElement.textContent = '00:00';
+  millisecondsElement.textContent = '.00';
 }
 
 // Helper function to format time values as two digits
 function formatTime(time) {
-  return time < 10 ? "0" + time : time;
+  return time < 10 ? '0' + time : time;
 }
 
 // Resets all bars exept 60th second bar to maintain the visual design
@@ -84,12 +106,8 @@ function resetBars(deleyTime) {
     const currentBarId = `barId${barNumber}`;
     setTimeout(() => {
       // Choose dark grey highlight color to every 5th bar to maintain the visual design
-      const currBarHighlightColor =
-        barNumber % 5 === 0
-          ? "var(--bar-secondary-color)"
-          : "var(--bar-primary-color)";
-      document.querySelector(`#${currentBarId} p`).style.backgroundColor =
-        currBarHighlightColor;
+      const currBarHighlightColor = barNumber % 5 === 0 ? 'var(--bar-secondary-color)' : 'var(--bar-primary-color)';
+      document.querySelector(`#${currentBarId} p`).style.backgroundColor = currBarHighlightColor;
     }, deleyTime * (59 - barNumber)); // Delay ensures a gradual reset effect
   }
 }
@@ -98,31 +116,32 @@ function resetBars(deleyTime) {
 function highlightCurrentBar(currentSecond) {
   const currentBarId = `barId${currentSecond}`;
   // Chooses blue highlight color to every 5th bar to maintain the visual design
-  const currBarHighlightColor =
-    currentSecond % 5 === 0 ? "var(--primary-color)" : "var(--secondary-color)";
-  document.querySelector(`#${currentBarId} p`).style.backgroundColor =
-    currBarHighlightColor;
+  const currBarHighlightColor = currentSecond % 5 === 0 ? 'var(--primary-color)' : 'var(--secondary-color)';
+  document.querySelector(`#${currentBarId} p`).style.backgroundColor = currBarHighlightColor;
   tickingSound.volume = 0.5;
   tickingSound.play(); // plays ticking sound when bar gets highlighted
 }
 
 // Switches themes and updates the icon accordingly
 function toggleTheme() {
-  const isDarkMode = body.classList.contains("dark");
-  body.classList.toggle("dark");
+  const toggleThemeButton = document.querySelector('.toggle-theme');
+  const isDarkMode = body.classList.contains('dark');
+  body.classList.toggle('dark');
 
   if (isDarkMode) {
-    themeToggleButton.innerHTML = `<svg id="moon" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#6e6e6e" viewBox="0 0 16 16">
-      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+    toggleThemeButton.innerHTML = `
+      <svg id="moon-icon" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#6e6e6e" viewBox="0 0 16 16">
+        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
       </svg>`;
   } else {
-    themeToggleButton.innerHTML = `<svg id="sun" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#bcbcbc" viewBox="0 0 16 16">
-      <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+    toggleThemeButton.innerHTML = `
+      <svg id="sun-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#bcbcbc" viewBox="0 0 16 16">
+        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
       </svg>`;
   }
 }
 
 // Adds entry animation to the stopwatch container on document load
-document.addEventListener("DOMContentLoaded", () => {
-  container.classList.add("animate-entry");
+document.addEventListener('DOMContentLoaded', () => {
+  mainContainer.classList.add('animate-entry');
 });
